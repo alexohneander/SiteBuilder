18eff427d0a589833aa4153c4dbfb4c8543ae4fc
Alex Wellnitz <moin@wellnitz-alex.de>
2019-10-31 15:05:12 +0100

    Changed Tenant Model

--- SiteBuilder.DataEntity/Models/Tenant.cs
+++ SiteBuilder.DataEntity/Models/Tenant.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace SiteBuilder.DataEntity.Models
 {
     public class Tenant
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public User User { get; set; }
     }
 }
