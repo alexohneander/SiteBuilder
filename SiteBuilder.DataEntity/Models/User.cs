90262f2db93167291f4fb74fdbb45f6d0696a601
Alex Wellnitz <moin@wellnitz-alex.de>
2019-10-31 15:05:26 +0100

    Add new User Model

---
+++ SiteBuilder.DataEntity/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiteBuilder.DataEntity.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
